36d0d3d3368a269ac735be601ebf0c7a82411fd9
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-07-06 10:39:37 +0800

    Header and Stylings

--- src/App.js
+++ src/App.js
@@ -2,50 +2,56 @@ import { useState, useEffect } from "react";
 import './App.css';
 
 function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
   });
   useEffect(() => {
-    // Handler to call on window resize
     function handleResize() {
-      // Set window width/height to state
       setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
       });
     }
-    // Add event listener
     window.addEventListener("resize", handleResize);
-    // Call handler right away so state gets updated with initial window size
     handleResize();
-    // Remove event listener on cleanup
     return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
+  }, []);
   return windowSize;
+}
+
+function Header() {
+  return (
+    <header className="header">
+
+    </header>
+  )
 }
 
 function App() {
   const winsize = useWindowSize();
   return (
     <div className="App">
-      <header>
-
-      </header>
-      <section>
+      <Header />
+      <section style={{ paddingTop: "36px" }}>
         <article>
-          <textarea 
-          style={{
-            width: winsize.width-21,
-            height: winsize.height-12,
-          }}
-          className="code-edit" />
+          <TextArea />
         </article>
       </section>
     </div>
   );
+}
+
+function TextArea() {
+  const winsize = useWindowSize();
+  return (
+    <textarea
+      style={{
+        width: winsize.width - 6,
+        height: winsize.height - 48,
+      }}
+      className="code-edit" />
+  )
 }
 
 export default App;
