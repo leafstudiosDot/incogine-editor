5e8e1a4720b0c16291397374bca9d6a75a4ac2ae
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-07-06 19:21:52 +0800

    Line Count

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <section style={{ paddingTop: "36px" }}>
+      <section >
         <article>
           <TextArea />
         </article>
@@ -44,13 +44,36 @@ function App() {
 
 function TextArea() {
   const winsize = useWindowSize();
+  const [text, setText] = useState("");
+  const [textline, settextline] = useState(1);
+
+  function handleTextChange(e) {
+    setText(e.target.value);
+    settextline(e.target.value.split("\n").length);
+  }
+
+  function onScroll(e) {
+    console.log(e.target.scrollTop)
+    document.getElementById("linecount-edit-cont").scrollTop = e.target.scrollTop;
+  }
+
   return (
-    <textarea
-      style={{
-        width: winsize.width - 6,
-        height: winsize.height - 48,
-      }}
-      className="code-edit" />
+    <div style={{ paddingTop: "36px" }}>
+      <div id="linecount-edit-cont" style={{ height: winsize.height - 45 }}>
+        {Array(textline).fill(1).map((_, i) => 
+          <div id="linecount-edit-num">{i + 1}</div>
+        )}
+      </div>
+      <textarea
+        style={{
+          width: winsize.width - 46,
+          height: winsize.height - 48
+        }}
+        onChange={handleTextChange}
+        value={text}
+        onScroll={onScroll}
+        id="code-edit" />
+    </div>
   )
 }
 
