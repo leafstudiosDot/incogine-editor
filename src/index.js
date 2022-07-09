f90bd840ae13a9bca345156072f618e28c7f4795
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-07-09 13:30:16 +0800

    Downgrade React
    "fs" PROBLEM so yeah, downgrade would fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { stopReportingRuntimeErrors } from "react-error-overlay";
+
+if (process.env.NODE_ENV === "development") {
+  stopReportingRuntimeErrors(); // disables error overlays
+}
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
