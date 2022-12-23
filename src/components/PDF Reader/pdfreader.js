a3e212346639106a328ad99fa1e73c70ee89e1e4
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-12-23 22:56:27 +0800

    Unnecessary Comments

--- src/components/PDF Reader/pdfreader.js
+++ src/components/PDF Reader/pdfreader.js
@@ -22,7 +22,6 @@ export default function PDFReader(props) {
 
     return (
         <div className="PDFReader" style={{ width: props.winsize.width, height: props.winsize.height - 56 }}>
-            {/*<div ref={containerRef} width={props.winsize.width} height={"100%"} />*/}
             <Document
 				file={props.docsState.docs[props.docsState.selected].content.file} onLoadSuccess={onDocumentLoadSuccess}>
 				<Page pageNumber={props.docsState.docs[props.docsState.selected].content.page}
