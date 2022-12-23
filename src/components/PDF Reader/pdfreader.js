ca36e1af0e167ba5a4906c208b3f86afbc200526
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2022-12-23 23:10:17 +0800

    Next and Previous Buttons

--- src/components/PDF Reader/pdfreader.js
+++ src/components/PDF Reader/pdfreader.js
@@ -20,6 +20,38 @@ export default function PDFReader(props) {
 		props.setDocs({ selected: props.docsState.selected, docs: [...oldprops] });
 	};
 
+    const goToPrevPage = () => {
+        oldprops[props.docsState.selected] = {
+            title: props.docsState.docs[props.docsState.selected].title,
+            file: props.docsState.docs[props.docsState.selected].file,
+            content: {
+                file: props.docsState.docs[props.docsState.selected].content.file,
+                page: props.docsState.docs[props.docsState.selected].content.page - 1 <= 1 ? 1 : props.docsState.docs[props.docsState.selected].content.page - 1,
+                totalpage: props.docsState.docs[props.docsState.selected].content.totalpage
+            },
+            saved: true,
+            type: "document/pdf",
+        }
+
+		props.setDocs({ selected: props.docsState.selected, docs: [...oldprops] });
+    }
+
+	const goToNextPage = () => {
+        oldprops[props.docsState.selected] = {
+            title: props.docsState.docs[props.docsState.selected].title,
+            file: props.docsState.docs[props.docsState.selected].file,
+            content: {
+                file: props.docsState.docs[props.docsState.selected].content.file,
+                page: props.docsState.docs[props.docsState.selected].content.page + 1 >= props.docsState.docs[props.docsState.selected].content.totalpage ? props.docsState.docs[props.docsState.selected].content.totalpage : props.docsState.docs[props.docsState.selected].content.page + 1,
+                totalpage: props.docsState.docs[props.docsState.selected].content.totalpage
+            },
+            saved: true,
+            type: "document/pdf",
+        }
+
+		props.setDocs({ selected: props.docsState.selected, docs: [...oldprops] });
+    }
+
     return (
         <div className="PDFReader" style={{ width: props.winsize.width, height: props.winsize.height - 56 }}>
             <Document
@@ -27,6 +59,13 @@ export default function PDFReader(props) {
 				<Page pageNumber={props.docsState.docs[props.docsState.selected].content.page}
                 height={props.winsize.height - 81} />
 			</Document>
+
+            <nav id="PDFReader_PDFNav">
+                <div id="PDFReader_PDFNav_Btns">
+                    <button id="PDFReader_PDFNav_Btn" onClick={goToPrevPage}>←</button>
+				    <button id="PDFReader_PDFNav_Btn" onClick={goToNextPage}>→</button>
+                </div>
+			</nav>
         </div>
     )
 }
