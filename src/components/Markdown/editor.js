78084eab123f2bcaec6d3e3926a1369eba965639
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-01-19 17:30:59 +0800

    Structuring

--- src/components/Markdown/editor.js
+++ src/components/Markdown/editor.js
@@ -5,7 +5,15 @@ import './editor.css';
 export default function MarkdownEdit(props) {
     return (
         <div className="markdown-edit">
-            
+            <div id="markdown-menubar">
+                
+            </div>
+            <div id="markdown-textbox">
+
+            </div>
+            <div id="markdown-preview">
+
+            </div>
         </div>
     )
 }
