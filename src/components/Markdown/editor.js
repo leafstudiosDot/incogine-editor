b8e3ff1995eebe321960c48b799319768ad2f711
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-01-21 10:23:04 +0800

    Importing Showdown.js

--- src/components/Markdown/editor.js
+++ src/components/Markdown/editor.js
@@ -1,8 +1,10 @@
 import { ipcRenderer } from 'electron';
 import { useState, useEffect, useCallback } from 'react';
+import showdown from 'showdown';
 import './editor.css';
 
 export default function MarkdownEdit(props) {
+    var converter = new showdown.Converter()
     return (
         <div className="markdown-edit">
             <div id="markdown-menubar">
@@ -12,7 +14,7 @@ export default function MarkdownEdit(props) {
 
             </div>
             <div id="markdown-preview">
-
+                
             </div>
         </div>
     )
