09b6c5e6983ad15207eab5d012569157981b4871
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-02-04 13:07:14 +0800

    Markdown Grid

--- src/components/Markdown/editor.js
+++ src/components/Markdown/editor.js
@@ -5,22 +5,25 @@ import './editor.css';
 
 export default function MarkdownEdit(props) {
     var converter = new showdown.Converter()
+    const [mdtext, setMDText] = useState("");
     return (
         <div className="markdown-edit">
             <div id="markdown-menubar">
                 
             </div>
             <div id="markdown-textbox">
-                <textarea id="markdown-textarea" onChange={(e) => {}}
+                <textarea id="markdown-textarea" onChange={(e) => {setMDText(e.target.value)}}
                 
                 style={{
-                    width: props.winsize.width - 46,
+                    width: (props.winsize.width - 46)/2,
                     height: props.winsize.height - 60 - props.titleMenuBarSpace
                   }}
                   />
             </div>
             <div id="markdown-preview">
+                <div id="markdown-preview-content" dangerouslySetInnerHTML={{__html: converter.makeHtml(mdtext)}}>
 
+                </div>
             </div>
         </div>
     )
