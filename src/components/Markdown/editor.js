5bae87909f999e9e4f9023e9953700d7460cae60
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-02-04 15:51:06 +0800

    Markdown WYSIWYG

--- src/components/Markdown/editor.js
+++ src/components/Markdown/editor.js
@@ -11,17 +11,20 @@ export default function MarkdownEdit(props) {
             <div id="markdown-menubar">
                 
             </div>
-            <div id="markdown-textbox">
+            <div id="markdown-textbox"
+                style={{
+                    width: (props.winsize.width - 46)/2,
+                }}>
                 <textarea id="markdown-textarea" onChange={(e) => {setMDText(e.target.value)}}
                 
                 style={{
-                    width: (props.winsize.width - 46)/2,
+                    width: ((props.winsize.width - 46)/2)-22,
                     height: props.winsize.height - 60 - props.titleMenuBarSpace
                   }}
                   />
             </div>
             <div id="markdown-preview">
-                <div id="markdown-preview-content" dangerouslySetInnerHTML={{__html: converter.makeHtml(mdtext)}}>
+                <div id="markdown-preview-content" style={{width: (props.winsize.width - 46)/2}} dangerouslySetInnerHTML={{__html: converter.makeHtml(mdtext)}}>
 
                 </div>
             </div>
