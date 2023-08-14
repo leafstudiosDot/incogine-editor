a6b410b8dda6d00bce2b624a564e3b980a857d3e
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-08-14 15:09:19 +0800

    Hyperlink and Realtime Preview Fixed

--- src/components/Markdown/editor.js
+++ src/components/Markdown/editor.js
@@ -1,6 +1,7 @@
 import { ipcRenderer } from 'electron';
 import { useState, useEffect, useCallback } from 'react';
 import showdown from 'showdown';
+import { shell } from 'electron';
 import './editor.css';
 
 export default function MarkdownEdit(props) {
@@ -9,27 +10,56 @@ export default function MarkdownEdit(props) {
 
     useEffect(() => {
         setMDText(props.docsState.docs[props.docsState.selected].content)
-    }, [props.docsState])
+    }, [])
+
+    useEffect(() => {
+
+        const md_cont = document.getElementById('markdown-preview-content')
+
+        const anchorTags = md_cont.getElementsByTagName('a');
+        for (const anchorTag of anchorTags) {
+            anchorTag.addEventListener('click', (event) => {
+                event.preventDefault();
+                const url = anchorTag.getAttribute('href');
+                shell.openExternal(url);
+            });
+        }
+
+    }, [mdtext, props.docsState])
+
+    function onTextAreaChange(changed) {
+        setMDText(changed)
+
+        let oldprops = [...props.docsState.docs];
+        oldprops[props.docsState.selected] = {
+            title: props.docsState.docs[props.docsState.selected].title,
+            file: props.docsState.docs[props.docsState.selected].file,
+            content: mdtext,
+            saved: false,
+            type: "markdown/edit",
+        }
+        props.setDocs({ selected: props.docsState.selected, docs: [...oldprops] })
+    }
 
     return (
         <div className="markdown-edit">
             <div id="markdown-menubar">
-                
+
             </div>
             <div id="markdown-textbox"
                 style={{
-                    width: (props.winsize.width - 46)/2,
+                    width: (props.winsize.width - 46) / 2,
                 }}>
-                <textarea id="markdown-textarea" value={mdtext} onChange={(e) => {setMDText(e.target.value)}}
-                
-                style={{
-                    width: ((props.winsize.width - 46)/2)-22,
-                    height: props.winsize.height - 60 - props.titleMenuBarSpace
-                  }}
-                  />
+                <textarea id="markdown-textarea" value={mdtext} onChange={(e) => { onTextAreaChange(e.target.value) }}
+
+                    style={{
+                        width: ((props.winsize.width - 46) / 2) - 22,
+                        height: props.winsize.height - 60 - props.titleMenuBarSpace
+                    }}
+                />
             </div>
             <div id="markdown-preview">
-                <div id="markdown-preview-content" style={{width: (props.winsize.width - 46)/2, height: (props.winsize.height - 61)}} dangerouslySetInnerHTML={{__html: converter.makeHtml(mdtext)}}>
+                <div id="markdown-preview-content" style={{ width: (props.winsize.width - 46) / 2, height: (props.winsize.height - 61) }} dangerouslySetInnerHTML={{ __html: converter.makeHtml(mdtext) }}>
 
                 </div>
             </div>
