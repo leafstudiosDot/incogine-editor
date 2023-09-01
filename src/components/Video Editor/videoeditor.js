8f13e64e49844a76b34a1a40c6118a1eae72d3c0
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-09-01 11:22:26 +0800

    Initializing Style

--- src/components/Video Editor/videoeditor.js
+++ src/components/Video Editor/videoeditor.js
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import './videoeditor.css'
 
 export default function VideoEditor(props) {
+    const [projectVideoDetail, setProjectVideoDetail] = useState({
+        name: "",
+        frametl: 0,
+        framerate: 24,
+
+    })
+
     return (
         <div className="videoEditor-container" style={{ width: props.winsize.width, height: props.winsize.height - 26 }}>
-            
+            <video className="videoEditor-video" controls></video>
         </div>
     )
 }
