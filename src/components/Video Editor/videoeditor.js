411a79254d9eaf9a36ce275cd659d1f8347935f1
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-09-01 12:45:47 +0800

    Added Video Editing WASM
    Migrating to Vite here we come

--- src/components/Video Editor/videoeditor.js
+++ src/components/Video Editor/videoeditor.js
@@ -1,17 +1,26 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import './videoeditor.css'
 
 export default function VideoEditor(props) {
-    const [projectVideoDetail, setProjectVideoDetail] = useState({
-        name: "",
-        frametl: 0,
-        framerate: 24,
 
-    })
+    useEffect(() => {
+        /*async function loadWasmModule() {
+            const wasm = import('./videoedit/pkg/incoedit_videoedit');
+            await wasm.default();
+
+            const wasmContainer = document.createElement('div');
+            wasmContainer.style.border = '1px solid black';
+            wasmContainer.style.padding = '10px';
+            wasmContainer.textContent = 'Loading Incogine Editor - Video Editor...';
+
+            const wasmModule = document.querySelector('.videoEditor-container');
+            wasmModule.appendChild(wasmContainer);
+        }
+        loadWasmModule();*/
+    }, []);
 
     return (
         <div className="videoEditor-container" style={{ width: props.winsize.width, height: props.winsize.height - 26 }}>
-            <video className="videoEditor-video" controls></video>
         </div>
     )
 }
