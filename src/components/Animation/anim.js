33f5036355835c94b0756f36b0d834520196d46e
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-09-14 13:28:51 +0800

    Add for Animation Branch

--- src/components/Animation/anim.js
+++ src/components/Animation/anim.js
@@ -4,7 +4,12 @@ import './anim.css'
 export default function VideoEditor(props) {
     return (
         <div className="anim-container" style={{ width: props.winsize.width, height: props.winsize.height - 26 }}>
-            <canvas></canvas>
+            <iframe style={{
+                width: "100%",
+                height: "100%",
+                position: "fixed",
+                border: "none"
+            }} title="Animation" src={`http://localhost:3619`} />
         </div>
     )
 }
